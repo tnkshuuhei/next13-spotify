6c46bd8d17d4e046f8741747825ffcd71fc66d55
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-09-06 22:52:00 +0900

    added server fetching

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,14 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
-export default function Home() {
+
+import PageContent from "./components/PageContent";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div
       className="
@@ -46,7 +54,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
         </div>
-        {/* <PageContent songs={songs} /> */}
+        <PageContent songs={songs} />
       </div>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,11 @@ import { useRouter } from "next/navigation";
 import { FaUserAlt } from "react-icons/fa";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-// import { useSupabaseClient } from "@supabase/auth-helpers-react";
-// import { toast } from "react-hot-toast";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { toast } from "react-hot-toast";
 
-// import useAuthModal from "@/hooks/useAuthModal";
-// import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
 // import usePlayer from "@/hooks/usePlayer";
 
 import Button from "./Button";
@@ -23,19 +23,18 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   // const player = usePlayer();
   const router = useRouter();
-  // const authModal = useAuthModal();
+  const authModal = useAuthModal();
 
-  // const supabaseClient = useSupabaseClient();
-  // const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
 
   const handleLogout = async () => {
-    // const { error } = await supabaseClient.auth.signOut();
+    const { error } = await supabaseClient.auth.signOut();
     // player.reset();
-    // router.refresh();
-
-    // if (error) {
-    //   toast.error(error.message);
-    // }
+    router.refresh();
+    if (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -117,7 +116,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
-        {/* <div className="flex justify-between items-center gap-x-4">
+        <div className="flex justify-between items-center gap-x-4">
           {user ? (
             <div className="flex gap-x-4 items-center">
               <Button onClick={handleLogout} className="bg-white px-6 py-2">
@@ -154,7 +153,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               </div>
             </>
           )}
-        </div> */}
+        </div>
       </div>
       {children}
     </div>
